f6f303d4eb2dbee086e3d68166488dcfe806d673
Intelleqt <Jckj0170@gmail.com>
2019-11-22 13:49:23 -0500

    Master Push

---
+++ Assets/Scriptes/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed;
+    private Transform playerPos;
+
+
+    void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+}
